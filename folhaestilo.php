e60fc8d1ac2915edc4cecfc4417d2d9c288de70e
Diogo Novais <dnovais17@gmail.com>
2015-01-09 16:04:55 +0000

    Criação da folha de estilo de codificação
    Criação da folha de estilo de codificação
    (folhaestilo.php)

---
+++ folhaestilo.php
@@ -0,0 +1,50 @@
+***** Folha de estilo para código em PHP *****
+
+<?php
+	/*
+		nome-do-ficheiro.php - Descrição do ficheiro
+		
+		 funções
+			|- função1(parametro1 , parametro2, parametro3)
+			|- função2(parametro1 , parametro2)
+			`- função1(parametro1 , parametro2, parametro3)
+	*/
+	
+	include "ficheiro1-a-incluir.php";
+	include "ficheiro2-a-incluir.php";
+	
+	class NomeDaClasse{
+		private $atributoDaClasse;
+		
+		// Construtor
+		function __construct(){
+			
+		}
+		
+		// Destrutor
+		function __destruct(){
+			
+		}
+		
+		public function outrosMetodosDoObjeto($parametro1, $parametro2, $parametro3){
+			
+			// Comentário de uma linha
+			instrução-de-exemplo;
+			
+			/*
+				Comentário
+				multi-linha
+			*/
+			
+			/****************************
+				Nota: Exemplo de nota
+			*****************************/
+			
+			/***************************************************
+				A FAZER: Exemplo de alguma tarefa a realizar
+			****************************************************/
+			
+			return $valor;
+		}
+	}
+?>
