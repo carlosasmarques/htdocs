e6405a39f5674f13aebdc994974f175be2ea32f6
Diogo Novais <dnovais17@gmail.com>
2015-01-14 17:32:07 +0000

    Alterações á classe de utilizador
    Alteração das variáveis de objeto de public para private
    Adição do atributo "fotografia" e respetivos getters e setters
    Correção da formatação do código
    Comentários

--- Utilizador.php
+++ Utilizador.php
@@ -1,153 +1,147 @@
 <?php
 
-class Utilizador{
-    public $nome;
-    public $numero;
-    public $id;
-    public $morada;
-    public $telefone;
-    public $activo;
-    public $tipoUtilizador;
-    public $username;
-    public $password;
-    public $dataRegisto;
-    public $dataNascimento;
-    public $funcao;
+	/*
+		utilizador.php - Representa um objeto do tipo utilizador
+		
+		 funções
+			|- construtor(dados do utilizador)
+			|- getters()
+			`- setters(atributo)
+	*/
 
-    function __construct($nome, $numero, $id, $morada, $telefone, $activo, $tipoUtilizador, $username, $password, $dataRegisto, $dataNascimento, $funcao){
-        $this->nome = $nome;
-        $this->numero = $numero;
-        $this->id = $id;
-        $this->morada = $morada;
-        $this->telefone = $telefone;
-        $this->activo = $activo;
-        $this->tipoUtilizador = $tipoUtilizador;
-        $this->username = $username;
-        $this->password = $password;
-        $this->dataRegisto = $dataRegisto;
-        $this->dataNascimento = $dataNascimento;
-        $this->funcao = $funcao;
-    }
+	class Utilizador{
+		private $nome;
+		private $numero;
+		private $id;
+		private $morada;
+		private $telefone;
+		private $activo;
+		private $tipoUtilizador;
+		private $username;
+		private $password;
+		private $dataRegisto;
+		private $dataNascimento;
+		private $funcao;
+		private $fotografia;
 
-    public function setActivo($activo)
-    {
-        $this->activo = $activo;
-    }
+		function __construct($nome, $numero, $id, $morada, $telefone, $activo, $tipoUtilizador, $username, $password, $dataRegisto, $dataNascimento, $funcao, $fotografia){
+			$this->nome = $nome;
+			$this->numero = $numero;
+			$this->id = $id;
+			$this->morada = $morada;
+			$this->telefone = $telefone;
+			$this->activo = $activo;
+			$this->tipoUtilizador = $tipoUtilizador;
+			$this->username = $username;
+			$this->password = $password;
+			$this->dataRegisto = $dataRegisto;
+			$this->dataNascimento = $dataNascimento;
+			$this->funcao = $funcao;
+			$this->fotografia = $fotografia;
+		}
 
+		public function setActivo($activo){
+			$this->activo = $activo;
+		}
 
-    public function setDataNascimento($dataNascimento)
-    {
-        $this->dataNascimento = $dataNascimento;
-    }
+		public function setDataNascimento($dataNascimento){
+			$this->dataNascimento = $dataNascimento;
+		}
 
-    public function getDataNascimento()
-    {
-        return $this->dataNascimento;
-    }
+		public function getDataNascimento(){
+			return $this->dataNascimento;
+		}
 
-    public function setDataRegisto($dataRegisto)
-    {
-        $this->dataRegisto = $dataRegisto;
-    }
+		public function setDataRegisto($dataRegisto){
+			$this->dataRegisto = $dataRegisto;
+		}
 
-    public function getDataRegisto()
-    {
-        return $this->dataRegisto;
-    }
+		public function getDataRegisto(){
+			return $this->dataRegisto;
+		}
 
-    public function setFuncao($funcao)
-    {
-        $this->funcao = $funcao;
-    }
+		public function setFuncao($funcao){
+			$this->funcao = $funcao;
+		}
 
-    public function getFuncao()
-    {
-        return $this->funcao;
-    }
+		public function getFuncao(){
+			return $this->funcao;
+		}
 
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
+		public function setId($id){
+			$this->id = $id;
+		}
 
-    public function getId()
-    {
-        return $this->id;
-    }
+		public function getId(){
+			return $this->id;
+		}
 
-    public function setMorada($morada)
-    {
-        $this->morada = $morada;
-    }
+		public function setMorada($morada){
+			$this->morada = $morada;
+		}
 
-    public function getMorada()
-    {
-        return $this->morada;
-    }
+		public function getMorada(){
+			return $this->morada;
+		}
 
-    public function setNome($nome)
-    {
-        $this->nome = $nome;
-    }
+		public function setNome($nome){
+			$this->nome = $nome;
+		}
 
-    public function getNome()
-    {
-        return $this->nome;
-    }
+		public function getNome(){
+			return $this->nome;
+		}
 
-    public function setNumero($numero)
-    {
-        $this->numero = $numero;
-    }
+		public function setNumero($numero){
+			$this->numero = $numero;
+		}
 
-    public function getNumero()
-    {
-        return $this->numero;
-    }
+		public function getNumero(){
+			return $this->numero;
+		}
 
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
+		public function setPassword($password){
+			$this->password = $password;
+		}
 
-    public function getPassword()
-    {
-        return $this->password;
-    }
+		public function getPassword(){
+			return $this->password;
+		}
 
-    public function setTelefone($telefone)
-    {
-        $this->telefone = $telefone;
-    }
+		public function setTelefone($telefone){
+			$this->telefone = $telefone;
+		}
 
-    public function getTelefone()
-    {
-        return $this->telefone;
-    }
+		public function getTelefone(){
+			return $this->telefone;
+		}
 
-    public function setTipoUtilizador($tipoUtilizador)
-    {
-        $this->tipoUtilizador = $tipoUtilizador;
-    }
+		public function setTipoUtilizador($tipoUtilizador){
+			$this->tipoUtilizador = $tipoUtilizador;
+		}
 
-    public function getTipoUtilizador()
-    {
-        return $this->tipoUtilizador;
-    }
+		public function getTipoUtilizador(){
+			return $this->tipoUtilizador;
+		}
 
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    }
+		public function setUsername($username){
+			$this->username = $username;
+		}
 
-    public function getUsername()
-    {
-        return $this->username;
-    }
+		public function getUsername(){
+			return $this->username;
+		}
 
-    public function getActivo()
-    {
-        return $this->activo;
-    }
-}
+		public function getActivo(){
+			return $this->activo;
+		}
+		
+		public function setFoto($fotografia){
+			$this->fotografia = $fotografia;
+		}
+		
+		public function getFoto(){
+			$return $this->fotografia;
+		}
+	}
 ?>
