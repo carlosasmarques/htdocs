983344b72afc7bbc8be20d4c87ef569a5ca4f6a2
Beludo <marco_beludo_09@hotmail.com>
2015-01-14 17:20:21 +0000

    Revert "Modificações"
    This reverts commit b6ee17f236ba47fcbac2c9779b33c117eb82c804.

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 include "login.php";
 
-$administrador = new GereAdministracao();
-if($administrador->listarUtilizadores() != NULL){
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,7 +34,12 @@ if($administrador->listarUtilizadores() != NULL){
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
+
 <body>
+
+
+
+
     <div class="container">
         <br>
         <br>
@@ -46,6 +49,7 @@ if($administrador->listarUtilizadores() != NULL){
         <br>
         <br>
         <br>
+
         <form class="form-signin" method="post">
             <h2 class="form-signin-heading">Autenticação</h2>
             <input type="text" class="form-control" placeholder="Utilizador" title="Introduza o seu nome de utilizador" name="username" autofocus>
@@ -58,6 +62,7 @@ if($administrador->listarUtilizadores() != NULL){
             </label>
             <a class="btn btn-lg btn-primary btn-block" title="Pressione para concluir a autenticação" onsubmit="login.php">Entrar</a>
         </form>
+
     </div>
     <!-- /container -->
 
@@ -65,40 +70,6 @@ if($administrador->listarUtilizadores() != NULL){
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-<?php
-    } else {
-?>  <div class="container">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-    <form class="form-signin" method="post">
-        <h2 class="form-signin-heading">Registar Administrador</h2>
-				<label class="form-control" for="nome">Nome</label><br />
-				<input class="form-control" type="text" name="nome" id="nome" size="40" maxlength="40" />
-				<label class="form-control" for="numero">Numero</label><br />
-				<input class="form-control" type="number" name="numero" id="numero" size="30" maxlength="30" />
-				<label class="form-control" for="password">Password</label><br />
-				<input class="form-control" type="password" name="password" id="password" size="40" maxlength="40" />
-				<label class="form-control" for="data-nascimento">Data de nascimento</label><br />
-				<input class="form-control" type="text" name="data-nascimento" id="data-nascimento" size="40" maxlength="40" />
-				<label class="form-control" for="funcao">Função</label><br />
-				<input class="form-control" type="text" name="funcao" id="funcao" size="40" maxlength="40" />
-				<label class="form-control" for="telefone">Telefone</label><br />
-				<input class="form-control" type="number" name="telefone" id="telefone" size="40" maxlength="40" />
-				<label class="form-control" for="morada">Morada</label><br />
-				<input class="form-control" type="text" name="morada" id="morada" size="40" maxlength="40" />
-				<label class="form-control" for="foto">Foto</label><br />
-				<input class="form-control" name="foto" id="foto" type="file" />
-        <a class="btn btn-lg btn-primary btn-block" title="Pressione para concluir o registo" onsubmit="GereAdministração.php">Registar</a>
-		</form>
-    </div>
-<?php
-    }
-?>
 </body>
+
 </html>
