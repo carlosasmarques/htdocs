d6316e8ac499f46556254ca17ba513c571d08ce2
Beludo <marco_beludo_09@hotmail.com>
2015-01-14 17:20:42 +0000

    Revert "Revert "Modificações""
    This reverts commit 983344b72afc7bbc8be20d4c87ef569a5ca4f6a2.

--- acessobd.php
+++ acessobd.php
@@ -2,7 +2,7 @@
 	/*
 		acessobd.php - classe para manipular a base de dados
 		
-		funções:
+		funï¿½ï¿½es:
 			|- construct($host, $db, $user, $pass)
 			|- destruct()
 			|- inserir($sql, $dados[] = null)
@@ -16,9 +16,9 @@
 	include "conf.php";
 
 	class BaseDados{
-		private $DBH;
+		public $DBH;
 		
-		// Ligação á base de dados
+		// Ligaï¿½ï¿½o ï¿½ base de dados
 		function __construct($host, $db, $user, $pass){
 			try{
 				$this->DBH = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
@@ -34,7 +34,7 @@
 			(assim que seja apagada a ultima referencia ao objeto)
 		*/
 		function __destruct(){
-			$this->LigacaoBD == null;
+			$this->DBH == null;
 		}
 		
 		// inserir dados numa tabela
@@ -133,5 +133,12 @@
 			}
 			return $dados;
 		}
+
+        public function getDBH()
+        {
+            return $this->DBH;
+        }
+
+
 	}
 ?>
