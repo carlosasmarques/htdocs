4d3ac9ca45197a6050cf84a9f6816eec2ed69042
Diogo Novais <dnovais17@gmail.com>
2015-01-15 18:25:33 +0000

    Alteração do login para mostrar erro de autenticação
    Alteração do login para mostrar erro de autenticação e remoção do
    formulário de registo do administrador

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+	// index.php - formulario de login dos utilizadores
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -41,10 +44,17 @@
         <br>
         <br>
         <br>
-        <form class="form-signin" method="post">
+        <form class="form-signin" action="login.php" method="post">
             <h2 class="form-signin-heading">Autenticação</h2>
             <input type="text" class="form-control" placeholder="Utilizador" title="Introduza o seu nome de utilizador" name="username" autofocus>
             <input type="password" class="form-control" placeholder="Palavra-passe" title="Introduza a sua palavra-passe de autenticação" name="password">
+			
+			<?php
+				if(isset($_GET["erro"]) && !empty($_GET["erro"]) && $_GET["erro"] == 1){
+					echo '<div class="alert alert-danger">Utilizador ou palavra-passe errados!</div>';
+				}
+			?>
+			
             <label class="checkbox">
                 <input type="checkbox" value="remember-me">Lembra-te de mim
             </label>
@@ -55,41 +65,5 @@
         </form>
     </div>
     <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-<div class="container">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-    <form class="form-signin" method="post">
-        <h2 class="form-signin-heading">Registar Administrador</h2>
-				<label class="form-control" for="nome">Nome</label><br />
-				<input class="form-control" type="text" name="nome" id="nome" size="40" maxlength="40" />
-				<label class="form-control" for="numero">Numero</label><br />
-				<input class="form-control" type="number" name="numero" id="numero" size="30" maxlength="30" />
-				<label class="form-control" for="password">Password</label><br />
-				<input class="form-control" type="password" name="password" id="password" size="40" maxlength="40" />
-				<label class="form-control" for="data-nascimento">Data de nascimento</label><br />
-				<input class="form-control" type="text" name="data-nascimento" id="data-nascimento" size="40" maxlength="40" />
-				<label class="form-control" for="funcao">Função</label><br />
-				<input class="form-control" type="text" name="funcao" id="funcao" size="40" maxlength="40" />
-				<label class="form-control" for="telefone">Telefone</label><br />
-				<input class="form-control" type="number" name="telefone" id="telefone" size="40" maxlength="40" />
-				<label class="form-control" for="morada">Morada</label><br />
-				<input class="form-control" type="text" name="morada" id="morada" size="40" maxlength="40" />
-				<label class="form-control" for="foto">Foto</label><br />
-				<input class="form-control" name="foto" id="foto" type="file" />
-        <a class="btn btn-lg btn-primary btn-block" title="Pressione para concluir o registo" onsubmit="GereAdministração.php">Registar</a>
-		</form>
-    </div>
-
 </body>
 </html>
