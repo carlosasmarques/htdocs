88826dbd608e3089d3452a9358b95dbc168a389b
Diogo Novais <dnovais17@gmail.com>
2015-01-19 16:43:33 +0000

    Ajuda para alterar a password do mysql

---
+++ alterar-password.php
@@ -0,0 +1,15 @@
+<?php
+	/*
+		Alterar a password de root
+		---------------------------
+		
+		1 - Na linha de comandos inserir
+			$ mysql -h 127.0.0.1 -u root -p
+			
+		2 - Inserir a password actual
+		
+		3 - Inserir
+			SET PASSWORD = PASSWORD('nova password');
+	
+	*/
+?>
