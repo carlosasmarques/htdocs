4288db6868a167ad889dd2da4516340f98e13b63
Diogo Novais <dnovais17@gmail.com>
2015-01-19 17:35:22 +0000

    Retificação do nome das variaveis de configuração para evitar confusões

--- conf.php
+++ conf.php
@@ -1,10 +1,10 @@
 <?php
 	// conf.php - Configurações da base de dados e do site
 
-	$servidor = "localhost";
-	$bd = "fmt";
-	$user = "root";
-	$pass = "1234";
-	$lingua = "pt-pt";
+	$conf_servidor = "localhost";
+	$conf_bd = "fmt";
+	$conf_user = "root";
+	$conf_pass = "1234";
+	$conf_lingua = "pt-pt";
 	// $outros = "valor";
 ?>
