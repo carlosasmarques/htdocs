ec9845abbe40b19bd674f6a48bab2c6817b91b82
Diogo Novais <dnovais17@gmail.com>
2015-01-20 14:58:02 +0000

    Novo teste de alteração de pessword

--- teste.php
+++ teste.php
@@ -1,5 +1,13 @@
 <?php
 	// teste.php - teste de funções/métodos individualmente
+	
+	include "daoutilizador.php";
+	
+	$daoutilizador = new DaoUtilizador();
+	
+	$daoutilizador->alterarPalavraPasse(4, "não quero password");
 
-	echo date_default_timezone_get();
+	//echo date_default_timezone_get();
+	
+	
 ?>
