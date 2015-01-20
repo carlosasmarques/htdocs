8ecf6a266faa11cbdb8bf3faac936b3932d6cf19
Diogo Novais <dnovais17@gmail.com>
2015-01-20 10:39:02 +0000

    Correção do ficheiro de configuração

--- conf.php
+++ conf.php
@@ -1,19 +1,9 @@
 <?php
 	// conf.php - Configurações da base de dados e do site
 
-<<<<<<< HEAD
-	$servidor = "localhost";
-	$bd = "fmt";
-	$user = "root";
-	$pass = "1234";
-	$lingua = "pt-pt";
-
-=======
 	$conf_servidor = "localhost";
 	$conf_bd = "fmt";
 	$conf_user = "root";
 	$conf_pass = "1234";
 	$conf_lingua = "pt-pt";
->>>>>>> origin/master
-	// $outros = "valor";
 ?>
