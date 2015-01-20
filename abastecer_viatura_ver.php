d4d0ec7bcc1d5184772144e5b2637619a568e586
TomeSilva <tom3lmsilva@gmail.com>
2015-01-20 16:43:20 +0000

    alterações dos links de html para php

--- abastecer_viatura_ver.php
+++ abastecer_viatura_ver.php
@@ -61,7 +61,7 @@
            <li class="drop-menu" style="z-index: 1">
                   <a>Utilizador <span class="caret"></span></a>
                   <ul class="sub-menu">
-                    <li><a href="alterar_utilizador.html">Alterar Password</a></li>
+                    <li><a href="alterar_utilizador.php">Alterar Password</a></li>
                     <li><a href="#">Sair</a></li>
                   </ul>
                </li>
@@ -77,34 +77,34 @@
 <ul>
    <li><a href="inicial.php"><span>Principal</span></a></li>
 		
-	  <li><a href="Gestao_stock_U.html"><span>Gerir Stocks</span></a>
+	  <li><a href="Gestao_stock_U.php"><span>Gerir Stocks</span></a>
    </li>
    	  <li class='has-sub'><a><span>Viaturas</span></a>
 		<ul>
-		  <li ><a href="gerir_viaturas.html"><span>Gerir Viaturas</span></a>
-          <li ><a href="manutencao.html"><span>Manutencoes</span></a>
-          <li ><a href="abastecer_viatura.html"><span>Abastecimentos</span></a>
-		  <li ><a href="gerir_inspecoes.html"><span>Inspeções</span></a>
+		  <li ><a href="gerir_viaturas.php"><span>Gerir Viaturas</span></a>
+          <li ><a href="manutencao.php"><span>Manutencoes</span></a>
+          <li ><a href="abastecer_viatura.php"><span>Abastecimentos</span></a>
+		  <li ><a href="gerir_inspecoes.php"><span>Inspeções</span></a>
 		</ul>
    </li>
 	  <li class='has-sub'><a><span>Transporte de Doentes</span></a>
 		<ul>
-		  <li ><a href="adicionar_transporte_doentes.html"><span>Adicionar Novo Transporte</span></a></li>
-		  <li ><a href="gerir_transporte_doentes.html"><span>Gerir Transporte de Doentes</span></a>
-		  <li><a href='gerir_utente.html'><span>Gerir Utentes</span></a>
+		  <li ><a href="adicionar_transporte_doentes.php"><span>Adicionar Novo Transporte</span></a></li>
+		  <li ><a href="gerir_transporte_doentes.php"><span>Gerir Transporte de Doentes</span></a>
+		  <li><a href='gerir_utente.php'><span>Gerir Utentes</span></a>
 		</ul>
    </li>
           
-        <li><a href='registo_consumos.html'><span>Registo de Consumos</span></a>
+        <li><a href='registo_consumos.php'><span>Registo de Consumos</span></a>
                                   
         </li>
     
     
    	    <li class='has-sub'><a><span>Mensagens</span></a>
             <ul>
-                <li ><a href='enviar_mensagem.html'><span>Nova Mensagem</span></a></li>
-				<li ><a href='mensagens_recebidas.html'><span>Mensagens Recebidas</span></a></li>
-				<li ><a href='mensagens_enviadas.html'><span>Mensagens Enviadas</span></a></li>
+                <li ><a href='enviar_mensagem.php'><span>Nova Mensagem</span></a></li>
+				<li ><a href='mensagens_recebidas.php'><span>Mensagens Recebidas</span></a></li>
+				<li ><a href='mensagens_enviadas.php'><span>Mensagens Enviadas</span></a></li>
 			</ul>
         </li>
     
@@ -115,7 +115,7 @@
 		
 		<ol class="breadcrumb" style="width: 800px">
 				<li><a href="inicial.php">Utilizador</a></li>
-				<li><a href="./abastecer_viatura.html">Gerir Abastecimentos</a></li>
+				<li><a href="./abastecer_viatura.php">Gerir Abastecimentos</a></li>
 				<li >Ver Abastecimentos de Viatura</li>
 			</ol>
 		  
@@ -176,7 +176,7 @@
 			</div>
 	
 		</div>	
-		  		<a href="./abastecer_viatura.html" class="btn btn-danger btn-xl pull-right" > Voltar </a>
+		  		<a href="./abastecer_viatura.php" class="btn btn-danger btn-xl pull-right" > Voltar </a>
 	</div>
 	</div>
     </div>
